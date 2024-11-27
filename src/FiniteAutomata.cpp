738a380b2bf63892a690d13cdccb04a7c488652b
Matthew <aquidmatthewla@gmail.com>
2024-11-27 06:09:08 +0000

    get transition tuples

--- src/FiniteAutomata.cpp
+++ src/FiniteAutomata.cpp
@@ -14,6 +14,7 @@ class FiniteAutomata {
         vector<unordered_map<char, vector<int>>> transition_func; 
         int init_state; 
         vector<int> accept_states;
+
  
         FiniteAutomata(vector<int> s, vector<char> a, vector<unordered_map<char, vector<int>>> t_f, int i, vector<int> a_s) {
             /*
@@ -30,6 +31,7 @@ class FiniteAutomata {
             init_state = i; 
             accept_states = a_s; 
         }
+
 
         bool process(string str) {
             
@@ -51,36 +53,49 @@ class FiniteAutomata {
             return false;
         }
 
-        /*
-        
-        For any state, q, in the NFA : Define E(q) = {q}
-        While there exists (p, e, r) in the transition function, with:
-            p = a state in E(q)
-            e = empty word arrow
-            r = a state not in E(q)
-        Do: E(q) + r
-        
-        */
 
     private: 
-       void convert_to_dfa() {
+        vector<tuple<int>> get_transition_tuples() {
+            /*
+                Convert transition function to a vector of (p, e, r)            
+            */
+
             vector<tuple<int>> nfa_transitions = {};
+
+            for (int i = 0; i < transition_func.size(); i++) {
+                unordered_map<char, vector<int>> s = transition_func[i]; 
+                for (auto c : s) {
+                    for (auto j : c.second) {
+                        nfa_transitions.push_back((i, c.first, j)); // (this state, letter, dest state)
+                    }
+                }
+            } 
+
+            return nfa_transitions;           
+        }    
+
+
+        void convert_to_dfa() {
+            vector<tuple<int>> nfa_transitions = get_transition_tuples();
             vector<tuple<int>> dfa_transitions = {};
 
 
             /*
-                convert the dictionary into a vector of tuples
-
+            For any state, q, in the NFA : Define E(q) = {q}
+            While there exists (p, e, r) in the transition function, with:
+                p = a state in E(q)
+                e = empty word arrow
+                r = a state not in E(q)
+            Do: E(q) + r
             */
-       
        } 
-
-            /*
-                States = {0, 1, 2}
-                Alphabet = {a, b}
-                transition func = { {a : {1}, b : {2}} , {a : {2}, b : {1}} , {a : {1}, b : {1}} }
-                init_state = 0
-                accept_states = {2}
-            */
-
 };
+
+
+/*
+    States = {0, 1, 2}
+    Alphabet = {a, b}
+    transition func = { {a : {1,2}, b : {2}} , {a : {2}, b : {1}} , {a : {1}, b : {1}} }
+    init_state = 0
+    accept_states = {2}
+*/
